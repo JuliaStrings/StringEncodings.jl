c8ac4208e3e7c70cb14b9042a7993b5a59c6c013
Milan Bouchet-Valat <nalimilan@club.fr>
2021-06-21 12:20:46 +0200

    Explain issue

--- test/runtests.jl
+++ test/runtests.jl
@@ -234,10 +234,9 @@ mktemp() do path, io
     @test_throws ArgumentError open(path, enc"ISO-2022-JP", true, false, false, false, true)
 end
 
-@testset "Shift JIS (issue #49)" begin
+@testset "Bytes written only when closing with Shift JIS (issue #49)" begin
     for enc in ("SHIFT_JIS", "SHIFT_JISX0213")
-        @test encode("ク", enc) ==
-            [0x83, 0x4e]
+        @test encode("ク", enc) == [0x83, 0x4e]
 
         b = IOBuffer()
         s = StringEncoder(b, enc)
